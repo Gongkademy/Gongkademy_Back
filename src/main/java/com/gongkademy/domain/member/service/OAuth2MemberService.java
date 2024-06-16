93950581ad5f851d146e7a6b841b73a13a19bccd
tnwls000 <tnwlssla20@naver.com>
2024-06-16 19:38:41 +0900

    Refactor: OAuth2MemberService 수정

--- src/main/java/com/gongkademy/domain/member/service/OAuth2MemberService.java
+++ src/main/java/com/gongkademy/domain/member/service/OAuth2MemberService.java
@@ -3,12 +3,7 @@ package com.gongkademy.domain.member.service;
 import com.gongkademy.domain.member.dto.PrincipalDetails;
 import com.gongkademy.domain.member.entity.Member;
 import com.gongkademy.domain.member.entity.MemberRole;
-import com.gongkademy.domain.member.dto.MemberInfoDTO;
-import com.gongkademy.domain.member.dto.MemberSignUpDTO;
-import com.gongkademy.domain.member.dto.MemberUpdateDTO;
 import com.gongkademy.domain.member.repository.MemberRepository;
-import com.gongkademy.global.exception.CustomException;
-import com.gongkademy.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -17,9 +12,6 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
 @Service
@@ -40,33 +32,33 @@ public class OAuth2MemberService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        // 이메일을 통해 사용자 식별
         String email = oAuth2User.getAttribute("email");
-
-        // 데이터베이스에서 멤버 조회
-        Optional<Member> existingMember = memberRepository.findByEmail(email);
-
-        Member member;
-        // 멤버가 데이터베이스에 존재하지 않으면 새로운 멤버 생성
-        if (existingMember.isEmpty()) {
-            member = Member.builder()
-                    .email(email)
-//                    .password("") // OAuth2 로그인 사용자는 비밀번호가 필요없음
-                    .nickname("") // 닉네임은 추후 업데이트 필요
-                    .birthday(null) // 생일 정보도 추후 업데이트 필요
-                    .university("")
-                    .major("")
-                    .minor("")
-                    .build();
-            member.addRole(MemberRole.USER);
-            memberRepository.save(member);
-        } else {
-            member = existingMember.get();
-        }
+        String name = oAuth2User.getAttribute("name");
+        Member member = getMember(email, name);
 
         // 기존 사용자 정보를 사용하여 OAuth2User 객체 반환
         return new PrincipalDetails(member, oAuth2User.getAttributes());
     }
 
-
+    private Member getMember(String email, String name) {
+        Member member;
+        Optional<Member> existingMember = memberRepository.findByEmail(email);
+        // 멤버가 데이터베이스에 존재하지 않으면 새로운 멤버 생성
+        if (existingMember.isEmpty()) {
+            member = Member.builder()
+                    .email(email)
+                    .name(name)
+                    .nickname("")
+                    .birthday(null)
+                    .university("")
+                    .major("")
+                    .minor("")
+                    .build();
+            member.addRole(MemberRole.GUEST); // 이후 회원가입을 위해 최초 로그인은 GUEST로 설정
+        } else {
+            member = existingMember.get();
+            member.updateName(name); // 멤버 정보 업데이트 되었다면 멤버 이름 업데이트
+        }
+        return memberRepository.save(member);
+    }
 }
