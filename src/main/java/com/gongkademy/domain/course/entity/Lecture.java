8d5a7f350ed2317cb63f78c10dafefe40b4b15cc
jhw <7115gpdnjs@naver.com>
2024-06-22 01:29:34 +0900

    refactor : lecture엔티티 연관관계및 비즈니스 로직 수정

--- src/main/java/com/gongkademy/domain/course/entity/Lecture.java
+++ src/main/java/com/gongkademy/domain/course/entity/Lecture.java
@@ -3,6 +3,7 @@ package com.gongkademy.domain.course.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
