e69b50ba6fdadd6b6068bbcbbc6b87a64501bb0e
pakrsevin <psv980817@naver.com>
2024-06-25 23:45:30 +0900

    Refactor: Notification 수정

--- src/main/java/com/gongkademy/domain/notification/entity/Notification.java
+++ src/main/java/com/gongkademy/domain/notification/entity/Notification.java
@@ -1,11 +1,23 @@
 package com.gongkademy.domain.notification.entity;
 
 import com.gongkademy.domain.member.entity.Member;
+import com.gongkademy.domain.member.entity.MemberRole;
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Notification {
 
     @Id
@@ -14,16 +26,22 @@ public class Notification {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
-    private Member member;
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member receiver;
 
-    private String type;
+    @Column(nullable = false)
+    private NotificationType type;
 
+    @Column(nullable = false)
     private Long articleId;
 
+    @Column(nullable = false)
     private String message;
 
+    @Column(nullable = false)
     private boolean isRead;
 
+    @Column(nullable = false)
     private LocalDateTime createDate;
 
 
