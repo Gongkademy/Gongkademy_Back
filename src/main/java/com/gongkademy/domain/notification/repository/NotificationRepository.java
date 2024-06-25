0d1b8b95d101d38110760da5770635a0e12bda7c
tnwls000 <tnwlssla20@naver.com>
2024-06-25 21:37:31 +0900

    Feat: NotificationRepository 작성

--- src/main/java/com/gongkademy/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/gongkademy/domain/notification/repository/NotificationRepository.java
@@ -1,4 +1,7 @@
 package com.gongkademy.domain.notification.repository;
 
-public interface NotificationRepository {
+import com.gongkademy.domain.notification.entity.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
