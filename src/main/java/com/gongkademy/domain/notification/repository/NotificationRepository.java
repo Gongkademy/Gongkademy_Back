88bbaca05aeb47c66598f5483ae8c11d38ce086b
pakrsevin <psv980817@naver.com>
2024-06-26 01:14:26 +0900

    Refactor: NotificationRepository 메소드 작성

--- src/main/java/com/gongkademy/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/gongkademy/domain/notification/repository/NotificationRepository.java
@@ -3,5 +3,12 @@ package com.gongkademy.domain.notification.repository;
 import com.gongkademy.domain.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
+        List<Notification> findByreceiverAndCreateDate(Long memberId, LocalDateTime now);
+    }
+
+
