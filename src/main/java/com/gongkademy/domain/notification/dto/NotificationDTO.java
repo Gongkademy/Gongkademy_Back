b062f6a8f19230c7a9a7e06bf9abcf343b4dad38
pakrsevin <psv980817@naver.com>
2024-06-26 00:13:27 +0900

    Refactor: NotificationDTO 코드 작성

--- src/main/java/com/gongkademy/domain/notification/dto/NotificationDTO.java
+++ src/main/java/com/gongkademy/domain/notification/dto/NotificationDTO.java
@@ -1,4 +1,26 @@
 package com.gongkademy.domain.notification.dto;
 
+import com.gongkademy.domain.notification.entity.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationDTO {
+
+    //알람 목록조회
+    private String receiver;
+    private NotificationType type;
+    private Long articleId;
+    private String message;
+    private boolean isRead;
+    private LocalDateTime create_date;
+
+
 }
