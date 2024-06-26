cf71e184b78ebdefbba7480b2f57160ceef7e7b1
tnwls000 <tnwlssla20@naver.com>
2024-06-27 00:55:06 +0900

    Feat: EmitterRepositoryImpl 작성

--- src/main/java/com/gongkademy/domain/notification/repository/EmitterRepositoryImpl.java
+++ src/main/java/com/gongkademy/domain/notification/repository/EmitterRepositoryImpl.java
@@ -1,4 +1,43 @@
 package com.gongkademy.domain.notification.repository;
 
-public class EmitterRepositoryImpl {
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class EmitterRepositoryImpl implements EmitterRepository {
+
+    private final Map<String, SseEmitter> emitterMap = new ConcurrentHashMap<>();
+    private final Map<String, Object> eventCache = new ConcurrentHashMap<>();
+
+    @Override
+    public SseEmitter save(String emitterId, SseEmitter sseEmitter) {
+        emitterMap.put(emitterId, sseEmitter);
+        return sseEmitter;
+    }
+
+    @Override
+    public void deleteById(String id) {
+        emitterMap.remove(id);
+    }
+
+    @Override
+    public void saveEventCache(String emitterId, Object event) {
+        eventCache.put(emitterId, event);
+    }
+
+    @Override
+    public Map<String, SseEmitter> findAllEmitterStartWithByMemberId(String memberId) {
+        return emitterMap.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(memberId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Override
+    public Map<String, Object> findAllEventCacheStartWithByMemberId(String memberId) {
+        return eventCache.entrySet().stream()
+                .filter(entry -> entry.getKey().startsWith(memberId))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 }
