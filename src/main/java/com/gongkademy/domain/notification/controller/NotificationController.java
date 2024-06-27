13858f1fab7b4bb310e8c08ad7a9de7f577cf943
tnwls000 <tnwlssla20@naver.com>
2024-06-27 23:48:06 +0900

    Feat: NotificationController 작성

--- src/main/java/com/gongkademy/domain/notification/controller/NotificationController.java
+++ src/main/java/com/gongkademy/domain/notification/controller/NotificationController.java
@@ -1,4 +1,26 @@
 package com.gongkademy.domain.notification.controller;
 
+import com.gongkademy.domain.member.dto.PrincipalDetails;
+import com.gongkademy.domain.notification.service.NotificationServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
 public class NotificationController {
+    private final NotificationServiceImpl notificationService;
+
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> connect(
+            @RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") final String lastEventId,
+            @AuthenticationPrincipal final PrincipalDetails principalDetails
+            ) {
+        return ResponseEntity.ok(notificationService.connect(principalDetails.getMemberId(), lastEventId));
+    }
 }
