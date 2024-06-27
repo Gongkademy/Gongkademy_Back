f7f1eaca18430f05ba3df29d7c24501593e1baa3
pakrsevin <psv980817@naver.com>
2024-06-28 01:39:22 +0900

    Refactor: NotificationService 코드 리팩토링

--- src/main/java/com/gongkademy/domain/notification/service/NotificationService.java
+++ src/main/java/com/gongkademy/domain/notification/service/NotificationService.java
@@ -1,14 +1,14 @@
 package com.gongkademy.domain.notification.service;
 
+import com.gongkademy.domain.notification.dto.request.NotificationRequestDTO;
 import com.gongkademy.domain.notification.entity.Notification;
 import com.gongkademy.domain.notification.entity.NotificationType;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface NotificationService {
 
-    Notification createNotification(Long memberId, NotificationType type, Long articleId, String message);
+    Notification createNotification(NotificationRequestDTO notificationRequestDTO);
     List<Notification> getNotifications(Long memberId);
     void changeReadStatus(Long notificationId);
 }
